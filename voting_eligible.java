7c608a1b2195b67788589f388806eb0e7e7f7cae
Jafeer Mohamed <jafeerjamal9@gmail.com>
2026-07-03 21:40:46 +0530

    Add files via upload

---
+++ voting_eligible.java
@@ -0,0 +1,15 @@
+package pratice;
+import java.util.Scanner;
+public class pd25 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int age=sc.nextInt();
+        if(age>=18){
+            System.out.println("eligiable for voting");
+        }
+        else{
+            System.out.println("not eligiable");
+        }
+    }
+    
+}
