2fa22d7d9904abd5db21229e7f950e8745961f23
Jafeer Mohamed <jafeerjamal9@gmail.com>
2026-07-03 21:30:17 +0530

    Update and rename pp2.java to arithmetic.java

--- arithmetic.java
+++ arithmetic.java
@@ -6,14 +6,15 @@ import java.util.Scanner;
 public class pp2 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
+        system.out.println("arithmetic operation");
         int n_1=sc.nextInt();
         int n_2=sc.nextInt();
-        sc.nextLine();
+        
         String oper=sc.nextLine();
         System.out.println("addition"+(n_1 + n_2));
         System.out.println("subrac"+(n_1 - n_2));
         System.out.println("multiplication"+(n_1 * n_2));
-        System.out.println("operation" +oper);
+       
 
     }
 }    
